51f7c62ad0f780740ed1d7b740cb41d1dde1f90c
Carlos <carloscoucho@gmail.com>
2019-03-30 22:36:53 +0100

    Task 2
    Crear una clase para hacer una conexion http.
    Crear una clase para hacer las tareas asincronas. Se vale del objeto anterior para recuperar los datos que necesita.
    El metodo asociado al boton, crea un AsyncTask que carga los datos en la interfaz si los datos JSON son correctos

--- app/src/main/java/rodriguezfernandez/carlos/whowroteit/FetchBook.java
+++ app/src/main/java/rodriguezfernandez/carlos/whowroteit/FetchBook.java
@@ -11,15 +11,17 @@ import org.json.JSONObject;
 
 import java.lang.ref.WeakReference;
 
-public class FetchBook extends AsyncTask <String, Void, String>{
+public class FetchBook extends AsyncTask<String, Void, String> {
     private WeakReference<TextView> mTitleText;
     private WeakReference<TextView> mAuthorText;
-    public FetchBook(TextView mTitleText, TextView mAuthorText){
+
+    public FetchBook(TextView mTitleText, TextView mAuthorText) {
         //Crear las referencias debiles a los elementos de la UI.
-        this.mTitleText=new WeakReference<>(mTitleText);
-        this.mAuthorText=new WeakReference<>(mAuthorText);
+        this.mTitleText = new WeakReference<>(mTitleText);
+        this.mAuthorText = new WeakReference<>(mAuthorText);
 
     }
+
     @Override
     protected String doInBackground(String... strings) {
         //Hacer la consulta a través de la clase NetworkUtils
@@ -30,9 +32,9 @@ public class FetchBook extends AsyncTask <String, Void, String>{
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
         try {
-            // Convert the response into a JSON object.
+            // Convertir la respuesta en un objeto JSON
             JSONObject jsonObject = new JSONObject(s);
-            // Get the JSONArray of book items.
+            // Extraer los "items" del libro en forma de JSONArray.
             JSONArray itemsArray = jsonObject.getJSONArray("items");
 
             // Initialize iterator and results fields.
@@ -40,42 +42,36 @@ public class FetchBook extends AsyncTask <String, Void, String>{
             String title = null;
             String authors = null;
 
-            // Look for results in the items array, exiting
-            // when both the title and author
-            // are found or when all items have been checked.
+            // Iterar los resultados obtenidos.
             while (i < itemsArray.length() &&
                     (authors == null && title == null)) {
-                // Get the current item information.
+                // Obtener los datos del libro.
                 JSONObject book = itemsArray.getJSONObject(i);
                 JSONObject volumeInfo = book.getJSONObject("volumeInfo");
 
-                // Try to get the author and title from the current item,
-                // catch if either field is empty and move on.
+                // Intentar capturar el titulo y el autor.
                 try {
                     title = volumeInfo.getString("title");
                     authors = volumeInfo.getString("authors");
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-                // Move to the next item.
+                // Iterar
                 i++;
             }
 
-            // If both are found, display the result.
+            // If tenemos ambos resultados, los ponemos en el UI
             if (title != null && authors != null) {
                 mTitleText.get().setText(title);
                 mAuthorText.get().setText(authors);
             } else {
-                // If none are found, update the UI to
-                // show failed results.
+                // Si no se logra, se muestran los mensajes de error.
                 mTitleText.get().setText(R.string.no_results);
                 mAuthorText.get().setText("");
             }
 
         } catch (Exception e) {
-            // If onPostExecute does not receive a proper JSON string,
-            // update the UI to show failed results.
+            // Si la cadena recibida no es JSON, mostramos errores en el UI.
             mTitleText.get().setText(R.string.no_results);
             mAuthorText.get().setText("");
         }
